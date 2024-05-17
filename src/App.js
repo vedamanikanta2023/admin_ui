df73a498916c16b0cc9773f381996583b22f6212
vedamanikanta <manikanta.vedam@gmail.com>
2024-05-17 11:08:21 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import AdminPage from './Components/adminPage';
+
+function App() {
+  return (
+    <div className="App">
+      <AdminPage />
+    </div>
+  );
+}
+
+export default App;
